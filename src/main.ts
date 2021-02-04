ef1b8faea9aa33fa799f946e7aecd3e175ce569c
sunzhiqiang <sun1358742@163.com>
2021-02-04 16:00:12 +0800

    下载 element-plus UI 库

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import router from './router'
 import store from './store'
 // 导入 全局样式
 import './assets/style/global.css'
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount('#app')
+
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(ElementPlus)
+
+app.mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="login-wrapper">
-
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 <script lang="ts">
